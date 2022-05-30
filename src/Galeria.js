34e24d90d518d57c18583090889d4deb6fd0e199
ArtN0nymous <jr616934@gmail.com>
2022-05-30 09:38:21 -0500

    imagenes galeria peliculas

---
+++ src/Galeria.js
@@ -0,0 +1,47 @@
+import { View, StyleSheet } from "react-native";
+export default function Galeria(){
+    var peliculas =[
+        {
+            nombre:'Detective Pikachu',
+            desc:'LIUAHDUIGYASFUDY',
+            url :'https://youtube.com',
+            src:'./img/detective-pikachu.jpg'
+        },
+        {
+            nombre:'Avengers Infinity War',
+            desc:'LIUAHDUIGYASFUDY',
+            url :'https://youtube.com',
+            src:'./img/Avengers-Infinity-War.jpg'
+        },
+        {
+            nombre:'Avengers Era de Ultron',
+            desc:'LIUAHDUIGYASFUDY',
+            url :'https://youtube.com',
+            src:'./img/Avengers-era-de-ultron.jpg'
+        },
+        {
+            nombre:'La era de hielo',
+            desc:'LIUAHDUIGYASFUDY',
+            url :'https://youtube.com',
+            src:'./img/la-era-de-hielo.jpg'
+        },
+        {
+            nombre:'Minions',
+            desc:'LIUAHDUIGYASFUDY',
+            url :'https://youtube.com',
+            src:'./img/minions.jpg'
+        }
+    ];
+    return(
+        <>
+            <View>
+            
+            </View>
+        </>
+    );
+}
+const styles = StyleSheet.create({
+    contenedor:{
+        flex:1
+    }
+});
