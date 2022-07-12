2d8ceaba95f87c82efcba55146426d5e512433da
ArtN0nymous <jr616934@gmail.com>
2022-07-12 17:07:43 -0500

    quité sdk de peinado

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 import {getAuth} from 'firebase/auth';
-/*const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyC-n4fR96D2J5GaTLiICHmNIvV7mCCCcMY",
   authDomain: "widdall.firebaseapp.com",
   projectId: "widdall",
@@ -11,16 +11,7 @@ import {getAuth} from 'firebase/auth';
   messagingSenderId: "1079934915434",
   appId: "1:1079934915434:web:cd8893adac70e9c8188b13",
   measurementId: "G-GSNBHKGD3V"
-};*/
-const firebaseConfig={
-  apiKey: "AIzaSyCe9AFCOQ1pf00xg2GYFSHdZ0R0WsCqHTg",
-    authDomain: "rinrin-7d3d4.firebaseapp.com",
-    projectId: "rinrin-7d3d4",
-    storageBucket: "rinrin-7d3d4.appspot.com",
-    messagingSenderId: "325462402381",
-    appId: "1:325462402381:web:ec22d6e9d1e2fdf28e8304",
-    measurementId: "G-QYX45K634S",
-}
+};
 var app = firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({ experimentalForceLongPolling: true, merge:true });
 const db = app.firestore();
